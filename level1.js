38ee8e9d6b9526cb84ac85dfb2aff978a9ba56ba
hamguy98 <hayden.kennedy@students.lemondenorman.org>
2026-02-26 08:26:59 -0600

    Update level1.js

--- level1.js
+++ level1.js
@@ -1,1 +1,15 @@
-
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+function draw2d(color, offsetX, offsetY, sizeX, sizeY) {
+  ctx.fillStyle = color;
+  ctx.fillRect(offsetX, offsetY, sizeX, sizeY);
+  return
+}
+     
+draw2d("red", 125, 495, 500, 9)
+     
+draw2d("gray", 100, 460, 70, 9)
+draw2d("gray", 200, 420, 70, 9)
+draw2d("gray", 300, 470, 70, 9)
+draw2d("gray", 450, 430, 70, 9)
+draw2d("gray", 550, 450, 60, 9)
