8980192eafdfe34cb1c8eab585cf4bc22a30d96b
hamguy98 <hayden.kennedy@students.lemondenorman.org>
2026-03-06 19:29:36 +0000

    Changed some more stuff.. yummy

--- Game/main.js
+++ Game/main.js
@@ -9,7 +9,7 @@ function draw2d(color, offsetX, offsetY, sizeX, sizeY) {
 
 //player stuff
 var player;
-player = draw2d("green", 0, 0, 30, 30)
+player = draw2d("green", 0, 0, 30, 30);
 function move(obj, X, Y) {
   console.log(obj);
 }
@@ -19,23 +19,18 @@ var startButton = document
   .querySelector("#startGame")
   .addEventListener("click", () => {
     this.startGame();
-});
+  });
 
+//start
 startGame = () => {
   console.log("Game is starting");
-  const canvas = document.getElementById("Canvas");
-  const ctx = canvas.getContext("2d");
-  function draw2d(color, offsetX, offsetY, sizeX, sizeY) {
-    ctx.fillStyle = color;
-    var block = ctx.fillRect(offsetX, offsetY, sizeX, sizeY);
-    return block
-  }
-       
-  draw2d("red", 125, 495, 500, 9)
-       
-  draw2d("gray", 100, 460, 70, 9)
-  draw2d("gray", 200, 420, 70, 9)
-  draw2d("gray", 300, 470, 70, 9)
-  draw2d("gray", 450, 430, 70, 9)
-  draw2d("gray", 550, 450, 60, 9)
+
+  //TODO: make the files do the thing
+  draw2d("red", 125, 495, 500, 9);
+
+  draw2d("gray", 100, 460, 70, 9);
+  draw2d("gray", 200, 420, 70, 9);
+  draw2d("gray", 300, 470, 70, 9);
+  draw2d("gray", 450, 430, 70, 9);
+  draw2d("gray", 550, 450, 60, 9);
 };
